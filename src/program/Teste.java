664d5cc167940b77c9d4a03cf8cc735db3a90716
BrehCore <brehcore@gmail.com>
2023-09-06 17:38:57 -0300

    First commit

---
+++ src/program/Teste.java
@@ -0,0 +1,18 @@
+package program;
+
+public class Teste {
+
+	public static void main(String[] args) {
+		
+		int valor1 = 2;
+		int valor2 = 6;
+		int resultado;
+		
+		resultado = valor1 + valor2;
+		
+		System.out.println("O valor da soma é: " + resultado);
+		
+
+	}
+
+}
